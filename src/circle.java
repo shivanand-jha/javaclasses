45a1cb7f2c7727e29e5c287255fe5798f572ffe7
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-02-01 10:31:02 +0530

    inheritance example

---
+++ src/circle.java
@@ -0,0 +1,24 @@
+public class circle {
+    double radius;
+    public double area ()
+    {
+        return Math.PI*radius*radius;
+    }
+    public double perimeter(){
+        return 2*Math.PI*radius;
+    }
+}
+class cylinder1 extends circle {
+    double height;
+    public double volume(){
+        return area()*height;
+    }
+    public static void main(String[] args) {
+        cylinder1 c = new cylinder1();
+        c.radius=7;
+        c.height=10;
+        System.out.println("volume : "+c.volume());
+        System.out.println("base area : "+c.area());
+        System.out.println("perimeter :"+c.perimeter());
+    }
+}
