b7c4cad2c2e1cd15cacae03cb569f80d3c4f00f7
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-02-09 22:22:35 +0530

    abstract rules

---
+++ src/absDemo.java
@@ -0,0 +1,17 @@
+public class absDemo {
+}
+
+abstract class su {
+    //it made for inheritance only;
+    //abstract and final never used together;
+    //object can't be created;
+    //abstract class cannot be static also;
+    // every method of abstract class should be overridden
+
+
+}
+
+
+
+
+
---
+++ src/doublyLinkedList.java
@@ -0,0 +1,49 @@
+//import java.util.Scanner;
+//class node{
+//    int roll;
+//    node previous;
+//    node next;
+//}
+//public class doublyLinkedList {
+//    doublyLinkedList(){
+//
+//    }
+//    void insert(){
+//        System.out.println("insert fn working");
+//    }
+//    void delete(){
+//        System.out.println("delete fn is working");
+//    }
+//    void traverse(){
+//        System.out.println("traverse fun is working");
+//        if (start==null){
+//
+//        }
+//    }
+//    public static void main(String[] args) {
+//        doublyLinkedList obj = new doublyLinkedList();
+//        while (true) {
+//            System.out.println("Press 1 for insert");
+//            System.out.println("Press 2 for delete");
+//            System.out.println("Press 3 for traverse");
+//            System.out.println("Press 4 for exit");
+//            Scanner sc = new Scanner(System.in);
+//            int choice = sc.nextInt();
+//            switch (choice) {
+//                case 1:
+//                    obj.insert();
+//                    break;
+//                case 2:
+//                    obj.delete();
+//                    break;
+//                case 3:
+//                    obj.traverse();
+//                    break;
+//                case 4:
+//                    System.exit(0);
+//                default:
+//                    System.out.println("wrong choice");
+//            }
+//
+//        }
+//    }
