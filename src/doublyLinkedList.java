64fb1a50ed0173206a135cd0bd6aabaa94729288
shivanand-jha <shivanand.jha_cs20@gla.ac.in>
2022-02-09 22:38:01 +0530

    Doubly linkedList

--- src/doublyLinkedList.java
+++ src/doublyLinkedList.java
@@ -1,49 +1,96 @@
-//import java.util.Scanner;
-//class node{
-//    int roll;
-//    node previous;
-//    node next;
-//}
-//public class doublyLinkedList {
-//    doublyLinkedList(){
-//
-//    }
-//    void insert(){
-//        System.out.println("insert fn working");
-//    }
-//    void delete(){
-//        System.out.println("delete fn is working");
-//    }
-//    void traverse(){
-//        System.out.println("traverse fun is working");
-//        if (start==null){
-//
-//        }
-//    }
-//    public static void main(String[] args) {
-//        doublyLinkedList obj = new doublyLinkedList();
-//        while (true) {
-//            System.out.println("Press 1 for insert");
-//            System.out.println("Press 2 for delete");
-//            System.out.println("Press 3 for traverse");
-//            System.out.println("Press 4 for exit");
-//            Scanner sc = new Scanner(System.in);
-//            int choice = sc.nextInt();
-//            switch (choice) {
-//                case 1:
-//                    obj.insert();
-//                    break;
-//                case 2:
-//                    obj.delete();
-//                    break;
-//                case 3:
-//                    obj.traverse();
-//                    break;
-//                case 4:
-//                    System.exit(0);
-//                default:
-//                    System.out.println("wrong choice");
-//            }
-//
-//        }
-//    }
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+}
+
+public class doublyLinkedList {
+    static Scanner sc = new Scanner(System.in);
+    Node head;
+
+    public static void main(String[] args) {
+        doublyLinkedList ob = new doublyLinkedList();
+        while (true) {
+            System.out.print("Press:\n" +
+                    "1 for Insert\n" +
+                    "2 for Delete\n" +
+                    "3 for Traverse\n" +
+                    "4 for exit()\n" +
+                    "Enter your Choice -> ");
+            int Choice = sc.nextInt();
+            switch (Choice) {
+                case 1:
+                    ob.insert();
+                    break;
+                case 2:
+                    ob.delete();
+                    break;
+                case 3:
+                    ob.traverse();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Wrong Choice! ");
+            }
+        }
+    }
+
+    doublyLinkedList() {
+        head = null;
+    }
+
+    void insert() {
+        System.out.print("Enter your Data -> ");
+        int data = sc.nextInt();
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next = null;
+        newNode.prev = null;
+        if (head == null)
+            head = newNode;
+        else {
+            Node currentNode = head;
+            while (currentNode.next != null)
+                currentNode = currentNode.next;
+            currentNode.next = newNode;
+            newNode.prev = currentNode;
+        }
+        System.out.println("Data Inserted!");
+        // System.out.println("Insert Fn is Working!");
+    }
+
+    void delete() {
+        System.out.println("Deleted -> " + head.data);
+        if (head == null)
+            System.out.println("List is Empty!");
+        if (head.next == null)
+            head = null;
+        else
+            head = head.next;
+        head.prev = null;
+        // System.out.println("Delete Fn is Working!");
+    }
+
+    void traverse() {
+        if (head == null)
+            System.out.println("List Empty!");
+        else {
+            // forward
+            Node currentNode;
+            Node lastNode = null;
+            for (currentNode = head; currentNode != null; currentNode = currentNode.next) {
+                System.out.print(currentNode.data + " ");
+                lastNode = currentNode;
+            }
+            System.out.println();
+            // reverse
+            for (; lastNode != null; lastNode = lastNode.prev)
+                System.out.print(lastNode.data + " ");
+            System.out.println();
+        }
+        // System.out.println("Traverse Fn is Working!");
+    }
+}
